26e7a69710f545689a46b0836e854b60d3f4af6e
kaitokidi <genis.bayona@est.fib.upc.edu>
2015-10-28 10:12:01 +0100

    Update GameEngine.h

--- src/GameEngine.h
+++ src/GameEngine.h
@@ -2,10 +2,10 @@
 
 #include <SFML/Graphics.hpp>
 
+#include "GUI.h"
+#include "GUIMenu.h"
 #include "GameData.h"
 #include "WorldManager.h"
-#include "GUIMenu.h"
-#include "GUI.h"
 
 
 class GameEngine
@@ -19,25 +19,29 @@ public:
 	void close();
 
 protected:
+	//Functions
+	void draw();
+	void display();
+	
 	void initialize();
 	void uninitialize();
+	
 	void loadContent();
 	void unloadContent();
 
 	void update(sf::Time &elapsedTime);
-	void updateMainMenu(sf::Time &elapsedTime);
 	void updateGame(sf::Time &elapsedTime);
-	void draw();
-	void display();
+	void updateMainMenu(sf::Time &elapsedTime);
 
 	void handleEvents(sf::Time &elapsedTime);
 	void handleKeyboardEvent(sf::Event &event, sf::Time &elapsedTime);
 
 protected:
+	//Variables
+	GUI gui;
+	GUIMenu *menu;
 	GameData data;
 	WorldManager world;
-	GUIMenu *menu;
-	GUI gui;
 	sf::RenderWindow *window;
 };
 
